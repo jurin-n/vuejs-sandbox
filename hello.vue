1a3e4d59a63ab99ee411bf69667c69d83a578d74
jurin-n <jurin.nakano@gmail.com>
2019-08-03 20:54:55 +0900

    単一ファイルコンポーネント試してみた。Vue.js入門 6.5。

---
+++ hello.vue
@@ -0,0 +1,15 @@
+<template>
+    <p class="message">メッセージ：{{ msg }}</p>
+</template>
+
+<script>
+export default {
+    data () {
+        return { msg: 'こんにちは！鷗外🎉'}
+    }
+}
+</script>
+
+<style>
+.message { color: #42b983; }
+</style>
