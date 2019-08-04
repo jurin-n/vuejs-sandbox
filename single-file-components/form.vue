c4675b3c0980f1d394b79308a800cf58d15ed83d
jurin-n <jurin.nakano@gmail.com>
2019-08-04 19:51:40 +0900

    名前つきスタイル識別子の算出プロパティを試してみたが、動かずorz Vue.js入門 6.6.3

---
+++ single-file-components/form.vue
@@ -0,0 +1,52 @@
+<template>
+    <form novalidate>
+        <p v-bind:class="alertValidation">{{ validMessage }}</p>
+        <textarea v-on:input="onInput" v-bind:class="textboxValidation"></textarea>
+    </form>
+</template>
+
+<script>
+export default {
+    data () {
+        return { valid: false }
+    },
+    computed: {
+        validMessage () {
+            return this.valid ? '入力されています。' : '入力されていません。'
+        },
+        alertValidation () {
+            return this.valid ? this.alert.success: this.alert.error
+        },
+        textboxValidation () {
+            return this.valid ? this.textbox.success : this.textbox.error
+        }
+    },
+    methods: {
+        isRequired (value) {
+            return value.length > 0
+        },
+        onInput (e) {
+            this.valid = this.isRequired(e.target.value)
+        }
+    }
+}
+</script>
+
+<style module="alert">
+.success {
+    color: green;
+}
+.error {
+    font-weight: bold;
+    color: red;
+}
+</style>
+
+<style module="textbox">
+.success {
+    border: soid 10px purple;
+}
+.error {
+    border: solid 10px orange;
+}
+</style>
