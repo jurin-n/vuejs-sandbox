799b6199ab62bac1ef359693c7e5eda6cb26f1e0
jurin-n <jurin.nakano@gmail.com>
2019-08-06 23:38:32 +0900

    Vuex試し始めた。 Vue.js入門p7.5.1。

---
+++ vuex-basic/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        テスト
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ vuex-basic/README.md
@@ -0,0 +1,9 @@
+### Vuexインストール
+```
+npm install vuex
+```
+
+### 起動
+```
+vue serve App.vue --open
+```
