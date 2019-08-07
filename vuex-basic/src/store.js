7c932768cf3bcb4295891f6c8514ea98f33f02d1
jurin-n <jurin.nakano@gmail.com>
2019-08-07 22:42:31 +0900

    タスク一覧を表示する対応。

--- vuex-basic/src/store.js
+++ vuex-basic/src/store.js
@@ -5,7 +5,20 @@ Vue.use(Vuex)
 
 //ストアの定義
 const store = new Vuex.Store({
-
+    state: {
+        tasks: [
+            {
+                id: 1,
+                name: '牛乳を買う',
+                done: true
+            },
+            {
+                id: 2,
+                name: 'Vue.jsの本を買う',
+                done: false
+            }
+        ]
+    }
 })
 
 //ストアをエクスポート
